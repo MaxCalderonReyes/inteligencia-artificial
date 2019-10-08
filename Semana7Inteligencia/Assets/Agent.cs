b986703cfd12e1023d60dc89b930ac66ca8cac8a
max <maxvalentino@hotmail.com>
2019-10-07 21:37:34 -0500

    CUalquierHUe4vada

---
+++ Semana7Inteligencia/Assets/Agent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public float MaxSteer;
+    public float MaxSpped;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 desired;
+    public Transform objetivo;
+
+    private Vector3 steer;
+
+    void Update()
+    {
+     
+        seek(this.gameObject, objetivo);
+    }
+    public void seek(GameObject actual, Transform objetivo)
+    {
+
+        desired = Vector3.zero;
+        desired = (objetivo.transform.position - actual.transform.position).normalized * MaxSpped;
+
+        steer = Vector3.ClampMagnitude(desired - velocity, MaxSteer);
+        velocity+=steer*Time.deltaTime;
+        transform.position+=velocity*Time.deltaTime;
+
+        transform.up =velocity;
+
+    }
+
+}
---
+++ Semana7Inteligencia/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour
+{
+  float timer=0;
+  float TimeTOWait=3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer>TimeTOWait){
+            timer=0;
+            Action() ;
+            RandomisePosition();
+        }
+        timer+=Time.deltaTime;
+    }
+    void Action(){
+Debug.Log("ACCION");
+    }
+     public void RandomisePosition(){
+        float X=Random.Range(-5,5);
+        float y=Random.Range(-5,5);
+        transform.position=new Vector3(X,y,0);
+    }
+}
